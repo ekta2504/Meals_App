7ffacbd35698ad499f55cdd9419d93e339767ba3
Ekta2504 <ekta.gangwani@seaflux.tech>
2023-02-22 12:17:27 +0530

    meals app

---
+++ lib/screens/favourite_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../models/meal.dart';
+import '../widgets/meal_item.dart';
+class FavoritesScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
+  FavoritesScreen(this.favoriteMeals);
+
+  @override
+  Widget build(BuildContext context) {
+    if (favoriteMeals.isEmpty){
+    return Center(child: Text('You have no favourites yet! - Start adding some'),);}
+    else{
+      ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            id: favoriteMeals[index].id,
+            title: favoriteMeals[index].title,
+            imageUrl: favoriteMeals[index].imageUrl,
+            duration: favoriteMeals[index].duration,
+            complexity: favoriteMeals[index].complexity,
+          );
+        },
+        itemCount: favoriteMeals.length,
+      );
+
+    }
+  }
+}
